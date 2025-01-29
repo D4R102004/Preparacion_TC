4547999af29c66fba608b700d479fdeb9c790ca8
D4R102004 <161531325+D4R102004@users.noreply.github.com>
2025-01-29 11:58:40 -0500

    solved swap
    Learn to use registers more, also swapm excersise solved

--- AC_CP4/swap.asm
+++ AC_CP4/swap.asm
@@ -2,13 +2,15 @@
 
 section .data
 ; declare variable, and initialize value
-x dq 157
-i db 2
-j db 6
+x dd 33
+i db 5
+j db 31
 section .bss
 ; uninitialized global and static variable
 ipow resd 1
 jpow resd 1
+ik resd 1
+jk resd 1
 
 section .text
 global main
@@ -19,52 +21,96 @@ main:
     xor ebx, ebx
     xor ecx, ecx
     xor edx, edx
-    mov ebx, 2 ; base
-    mov ecx, i ; exponent
+    movzx eax, byte [i]
     push eax
-    push ebx
-    push ecx
-    push edx
     call exp
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov dword [ipow], edx
-    mov ecx, j ; exponent
+    add esp, 4 ; Limpia i
+    mov [ipow], eax
+    mov [ik], eax
+    movzx eax, byte [j]
     push eax
-    push ebx
-    push ecx
-    push edx
     call exp
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov dword [jpow], edx
-    PRINT_UDEC 1, [ipow]
-    PRINT_UDEC 1, [jpow]
-    
+    add esp, 4; Limpia j
+    mov [jpow], eax
+    mov [jk], eax
+    ; Zerofy i
+    mov eax, [ipow]
+    and eax, [x]
+    push eax
+    call zerofy
+    add esp, 4
+    mov [ipow], eax
+    ; Zerofy j
+    mov eax, [jpow]
+    and eax, [x]
+    push eax
+    call zerofy
+    add esp, 4
+    mov [jpow], eax
+    ; Tabla verdad
+    mov eax, [ipow]
+    mov ebx, [jpow]
+    mov ecx, [x]
+    cmp eax, ebx
+    jl ij
+    jg ji
+    jmp end
+ij:
+    add ecx, [ik]
+    sub ecx, [jk]
+    jmp end
+ji:
+    add ecx, [jk]
+    sub ecx, [ik]
+end:
+    PRINT_UDEC 4, ecx
     ret
-exp:
+    
+    
+    
+    
+ zerofy:
+    ; Prologue
+    ; ----------
     push ebp
     mov ebp, esp
-    sub esp, 4
-    mov dword [ebp - 4], 0 ; exponent
-loop_start:
-    mov eax, [ebp - 4]
-    cmp eax, [ebp + 4]
-    je fin
-    mov edx, [ebp + 8]
-    add edx, edx
-    mov eax, [ebp - 4]
-    inc eax
-    jmp loop_start
-fin:
-    mov edx, [ebp + 8]
+    ; -----------
+    mov eax, [ebp + 8]
+    cmp eax, 1
+    jl fin
+    mov eax, 1
+ fin:
+    ; Epilogue
+    ; -----------
     mov esp, ebp
     pop ebp
     ret
-     
+    ; -----------
     
+
+exp:
+    ; Prologue
+    ; -----------
+    push ebp
+    mov ebp, esp
+    ; -----------
+    push ecx
+    push ebx
+    ; variable esta en ebp + 4
+    mov ecx, [ebp + 8]
+    mov ebx, 1
+while:
+    add ebx, ebx
+    dec ecx
+    cmp ecx, 0
+    jg while
+    mov eax, ebx
+    pop ebx
+    pop ecx
+    ; Epilogue
+    ; ------------
+    mov esp, ebp
+    pop ebp
+    ret
+    ; ----------
     
